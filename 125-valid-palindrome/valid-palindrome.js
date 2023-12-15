42a282ea4a520dcacb751c74423f0b66b166845d
FEBIN JOSEPH <128711401+Febin-Joseph@users.noreply.github.com>
2023-12-15 17:01:26 +0530

    Time: 63 ms (58.50%) | Memory: 46.8 MB (36.56%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanedString === cleanedString.split('').reverse().join('');
+};
