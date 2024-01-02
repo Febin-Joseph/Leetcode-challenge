0b93ddbcabb88b85df9dcbb5abf7fd3312dbb111
FEBIN JOSEPH <128711401+Febin-Joseph@users.noreply.github.com>
2024-01-02 20:41:37 +0530

    Time: 82 ms (8.23%) | Memory: 42.5 MB (27.25%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseOnlyLetters = function(s) {
+    const english = char => /^[a-zA-Z]$/.test(char)
+
+    let left = 0;
+    let right = s.length - 1;
+    let chars = s.split('');
+
+    while (left < right) {
+        if (!english(chars[left])) {
+            left++
+        } else if (!english(chars[right])) {
+            right--
+        } else {
+            [chars[left], chars[right]] = [chars[right], chars[left]];
+            left++;
+            right--;
+        }
+    }
+    return chars.join('')
+};
